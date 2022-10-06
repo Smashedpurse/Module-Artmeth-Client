8f3c757cc37e1da3985eb8b652d7869a68f74ae2
Ivan Rubio <smashedpurse@gmail.com>
2022-10-06 17:24:29 -0500

    first upload

--- src/api/service.js
+++ src/api/service.js
@@ -2,7 +2,7 @@ import axios from "axios";
  
 const api = axios.create({
   // make sure you use PORT = 5005 (the port where our server is running)
-  baseURL: "http://localhost:5005/api"
+  baseURL: `${process.env.REACT_APP_SERVER_URL}/gallery`
   // withCredentials: true // => you might need this option if using cookies and sessions
 });
  
